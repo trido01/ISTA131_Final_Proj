6f322f19289433632e10e1f8a855646ae558a4c7
ibrafiq <160310144+ibrafiq@users.noreply.github.com>
2024-04-28 21:58:25 -0700

    Add files via upload

--- Final_Proj.py
+++ Final_Proj.py
@@ -20,18 +20,18 @@ def bar_chart():
     goals = top_10.iloc[:, 9]
 
     
-    plt.figure(figsize=(12, 7))
+    plt.figure(figsize=(12, 7), facecolor='#D3D3D3')
     width = 0.5  
 
     
-    plt.bar(players_top10, goals, width=width, label='Goals scored', alpha=0.6)
+    plt.bar(players_top10, goals, width=width, label='Goals scored', alpha= 1, color = '#003399')
 
 
-    plt.xlabel('Player names')
-    plt.ylabel('Total Number of goals scored')
-    plt.title('Top 10 goal scorers in Premier League 23/24 Season')
-    plt.xticks(rotation=30)  
-    plt.yticks()
+    plt.xlabel('Player names', color = '#003399', fontweight = 'bold')
+    plt.ylabel('Total Number of goals scored', color = '#003399', fontweight = 'bold')
+    plt.title('Top 10 goal scorers in Premier League 23/24 Season', color = '#003399', fontweight = 'bold')
+    plt.xticks(rotation=30,fontsize= 8,fontweight = 'bold')  
+    plt.yticks(np.arange(0, goals.max() + 1,1),fontweight = 'bold')
     plt.legend()
     
     plt.show(block = False)
@@ -41,7 +41,7 @@ def scatter_plt():
     data = pd.read_csv('player_shooting_2023_2024.csv')
 
     
-    top_50 = data.nlargest(30, data.columns[9])  
+    top_50 = data.nlargest(40, data.columns[9])  
 
     top_10 = data.nlargest(10, data.columns[9])
     
@@ -50,65 +50,57 @@ def scatter_plt():
     goals = top_50.iloc[:, 9]  
 
     
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=(10, 5), facecolor='#D3D3D3' )
 
     
-    plt.scatter(SOT_played, goals, color='green', alpha=0.6, s=80)  
+    plt.scatter(SOT_played, goals, color='#0099cc', alpha=0.6, s=80)  
 
     
     slope, intercept = np.polyfit(SOT_played, goals, 1)
     x_values = np.array([min(SOT_played), max(SOT_played)])
     y_values = intercept + slope * x_values
 
-    plt.plot(x_values, y_values, 'b--', label='Regression Line')
+    plt.plot(x_values, y_values, 'b--', label='Regression Line', )
 
     
     for i, txt in enumerate(players_top10):
         plt.annotate(txt, (SOT_played.iloc[i], goals.iloc[i]), textcoords="offset points", xytext=(0,10), ha='center')
     
-    plt.xlabel('Total Shots Taken')
-    plt.ylabel('Goals Scored')
-    plt.title("Player's total Shoots vs Goals Scored Premier League 23/24 Season")
+    plt.xlabel('Total Shots Taken', color = '#003399', fontweight = 'bold')
+    plt.ylabel('Goals Scored', color = '#003399', fontweight = 'bold')
+    plt.title("Player's total Shoots vs Goals Scored Premier League 23/24 Season", color = '#003399', fontweight = 'bold')
     plt.legend()
-    plt.show()
+    plt.show(block = False)
 
 
-
-
-""" line graph"""
-def line_plt():
+def line2():
     data = pd.read_csv('player_shooting_2023_2024.csv')
+    data_team = pd.read_csv('EPL Standings 2000-2022.csv')
+    man_city_data = data_team.loc[data_team.iloc[:, 2] == 'Manchester City']
+    top_10 = data.nlargest(1, data.columns[9])
 
-    top_10 = data.nlargest(10, data.columns[9]) 
-
-    players_top10 = top_10.iloc[:, 2]  
-    SOT_played = top_10.iloc[:, 11] 
-    goals = top_10.iloc[:, 10]  
-
-    
-    sorted_data = top_10.sort_values(by=top_10.columns[11])
-    sorted_players = sorted_data.iloc[:, 2]
-    sorted_SOT = sorted_data.iloc[:, 11]
-    sorted_goals = sorted_data.iloc[:, 10]
-
-    plt.figure(figsize=(12, 8))
-
-    plt.plot(sorted_SOT, sorted_goals, linestyle='-', color='red', label='Goals vs SOT')
+    last_10_seasons = man_city_data.iloc[-10:, :]
 
 
+    seasons = last_10_seasons.iloc[:, 0]
+    goals_scored = last_10_seasons.iloc[:, 7]
 
-    plt.xlabel('Shots On Target')
-    plt.ylabel('Total Shots')
-    plt.title('Player shots on targets in Premier League 23/24 Season')
+    plt.figure(figsize=(10, 6), facecolor= 'skyblue')
+    plt.plot(seasons, goals_scored, linestyle='-', marker='o', color='navy', label='Goals Scored')
+
+    plt.xlabel('Season', color = 'white', fontweight = 'bold')
+    plt.ylabel('Goals Scored (GF)', color = 'white', fontweight = 'bold')
+    plt.title('Manchester City Goals Scored in the Last 10 Seasons', color = 'white', fontweight = 'bold' )
     plt.legend()
-
+    plt.xticks(rotation=45, fontweight = 'bold')  
+    plt.tight_layout()  
     plt.show()
 
 def main():
     
     bar_chart()
     scatter_plt() 
-    line_plt() 
+    line2()
 
 if __name__ == "__main__":
     main()
